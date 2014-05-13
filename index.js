f2d63c4fd2b09f38823bb51612f3751d6d4861c2
Janpo <janpo.pan@gmail.com>
2014-05-14 05:05:22 +0800

    to 0.1.0

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require('./lib/alipay');
+var alipay = {};
+alipay.AlipayNotify = require('./lib/alipay_notify.class').AlipayNotify;
+alipay.AlipaySubmit = require('./lib/alipay_submit.class').AlipaySubmit;
+module.exports = alipay;
